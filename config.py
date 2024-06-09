a2b719e17ab3d6f97a3956b48ff22ed84539374b
ElStevenn <sfukinguay@gmail.com>
2024-06-09 10:18:20 +0200

    Implemented setup-db.sh script where the user can create its database in localhost

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +17,18 @@ CSRF_SESSION_KEY = SECRET_KEY
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-engine = create_engine('mysql://' + DB_USER + ':' + DB_PASS + '@localhost/' + DB_NAME)
-SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASS + '@localhost/' + DB_NAME
+engine = create_engine('mysql://' + DB_USER + ':' + DB_PASS + '@182.18.0.2/' + DB_NAME)
+SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASS + '@182.18.0.2/' + DB_NAME
 
 Session = sessionmaker(bind=engine)
+
+def get_all_db_names():
+    """Sample function to show whether the function works or nit"""
+    with engine.connect() as connection:
+        result = connection.execute(text("SHOW DATABASES"))
+        databases = [row[0] for row in result]
+    return databases
+
+
+if __name__ == '__main__':
+    print(get_all_db_names())
--- scripts/setup-db.sh
+++ scripts/setup-db.sh
@@ -2,30 +2,30 @@
 
 echo -e "Set Up database\n"
 env_file="../.env"
-
+export DOCKER_HOST=unix:///var/run/docker.sock
 
 # Function to update .env
 update_env() {
     local key=$1
     local value=$2
     if grep -q "^$key=" "$env_file"; then
-      sed -i "s/^$key=.*/$key=$value/" "$env_file"
+        sed -i "s/^$key=.*/$key=$value/" "$env_file"
     else
-      echo "$key=$value" >> "$env_file"
+        echo "$key=$value" >> "$env_file"
     fi
-  }
+}
 
 # Ask for the database credentials
-echo "Create the db user"
+echo "Create the db user:"
 read db_user
 
-echo "Create the db name"
+echo "Create the db name:"
 read db_name
 
-echo "Create the db password"
-read db_password
+echo "Create the db password:"
+read -s db_password
 
-echo "debug (true|false)"
+echo "debug (true|false):"
 read debug
 
 # Create volume if it doesn't exist
@@ -39,10 +39,9 @@ if ! docker network ls | grep -q custom-isolated-network; then
 fi
 
 # Check if the .env file exists in the parent directory
-
 if [ ! -e "$env_file" ]; then
-  echo ".env file does not exist, creating it."
-  cat <<EOF > "$env_file"
+    echo ".env file does not exist, creating it."
+    cat <<EOF > "$env_file"
 # Database configuration
 DEBUG=$debug
 SECRET_KEY=hola
@@ -52,18 +51,34 @@ DB_PASS=$db_password
 EOF
 fi
 
-# Asign variables
+# Assign variables
 update_env "DEBUG" "$debug"
 update_env "SECRET_KEY" "hola"
 update_env "DB_NAME" "$db_name"
 update_env "DB_USER" "$db_user"
 update_env "DB_PASS" "$db_password"
 
-
-echo "Run developerment database container?(y/n)"
+echo "Run development database container? (y/n)"
 read create_db
-if [ ${create_db} == "y" ]; then
-  docker run -d  --name some-postgres -p 8000:5432 -e POSTGRES_PASSWORD="$db_password" -v data_volume:/var/lib/docker/volumes/data_volume 
+if [ "$create_db" == "y" ]; then
+    if docker container ls -a | grep -q some-postgres; then
+        docker container rm -f some-postgres
+    fi
+    docker run -d --name some-postgres -p 8000:5432 -e POSTGRES_PASSWORD="$db_password" -v data_volume:/var/lib/postgresql/data postgres
+    container_id=$(docker ps -aqf "name=some-postgres")
+    container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container_id")
+    container_port=$(docker inspect -f '{{ (index (index .HostConfig.PortBindings "5432/tcp") 0).HostPort }}' "$container_id")
+    echo "Container 'some-postgres' is running."
+    echo "Container IP Address: $container_ip"
+    echo "Container Port: $container_port"
+else
+    docker container start some-postgres
+    container_id=$(docker ps -aqf "name=some-postgres")
+    container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container_id")
+    container_port=$(docker inspect -f '{{ (index (index .HostConfig.PortBindings "5432/tcp") 0).HostPort }}' "$container_id")
+    echo "Container 'some-postgres' has been started."
+    echo "Container IP Address: $container_ip"
+    echo "Container Port: $container_port"
+fi
 
-
-
+docker container ls
